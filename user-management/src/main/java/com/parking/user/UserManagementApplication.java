0774be97ceff416733a96f7884303a83b808221b
Angel Joseph <josephin.angel@gmail.com>
2019-10-08 21:13:26 +0530

    Joseph|HIbernate JPA issue fix

--- user-management/src/main/java/com/parking/user/UserManagementApplication.java
+++ user-management/src/main/java/com/parking/user/UserManagementApplication.java
@@ -2,10 +2,11 @@ package com.parking.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(exclude= HibernateJpaAutoConfiguration.class)
 @EnableJpaRepositories(basePackages = "com.parking.user.repositories")
 public class UserManagementApplication {
 
